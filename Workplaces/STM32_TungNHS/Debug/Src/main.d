7e1fcff7d9f3f0c316b6604ca306d8ac9b150af9
tungnhs <tungnhs2014@gmail.com>
2024-10-10 23:01:07 +0700

    add manual

--- Workplaces/STM32_TungNHS/Debug/Src/main.d
+++
@@ -1,7 +0,0 @@
-Src/main.o: ../Src/main.c \
- D:/Learning/STM32_Legacy/STM32F407VG-Legacy_Learning/Workplaces/STM32_TungNHS/Drivers/Inc/stm32f407xx.h \
- D:/Learning/STM32_Legacy/STM32F407VG-Legacy_Learning/Workplaces/STM32_TungNHS/Drivers/Inc/stm32f407xx_gpio_driver.h \
- D:/Learning/STM32_Legacy/STM32F407VG-Legacy_Learning/Workplaces/STM32_TungNHS/Drivers/Inc/stm32f407xx.h
-D:/Learning/STM32_Legacy/STM32F407VG-Legacy_Learning/Workplaces/STM32_TungNHS/Drivers/Inc/stm32f407xx.h:
-D:/Learning/STM32_Legacy/STM32F407VG-Legacy_Learning/Workplaces/STM32_TungNHS/Drivers/Inc/stm32f407xx_gpio_driver.h:
-D:/Learning/STM32_Legacy/STM32F407VG-Legacy_Learning/Workplaces/STM32_TungNHS/Drivers/Inc/stm32f407xx.h:

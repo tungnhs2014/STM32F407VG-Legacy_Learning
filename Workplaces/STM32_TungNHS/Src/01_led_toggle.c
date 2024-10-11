16cbac62b539cf2e9215656bd8bbeb38505e1b52
tungnhs <tungnhs2014@gmail.com>
2024-10-11 10:35:45 +0700

    Add 01-led-toggle

---
+++ Workplaces/STM32_TungNHS/Src/01_led_toggle.c
@@ -0,0 +1,40 @@
+/*
+ * 01_led_toggle.c
+ *
+ *  Created on: Oct 11, 2024
+ *      Author: TungNHS
+ */
+
+#include "stm32f407xx.h"
+
+
+void delay(void)
+{
+	for(uint32_t i = 0 ; i < 500000 ; i ++);
+}
+
+
+int main(void)
+{
+
+	GPIO_Handle_t GpioLed;
+
+	GpioLed.pGPIOx = GPIOD;
+	GpioLed.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_12;
+	GpioLed.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUT;
+	GpioLed.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
+	GpioLed.GPIO_PinConfig.GPIO_PinOPType = GPIO_OP_TYPE_OD;
+	GpioLed.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_NO_PUPD;
+
+	GPIO_PeriClockControl(GPIOD,ENABLE);
+
+	GPIO_Init(&GpioLed);
+
+	while(1)
+	{
+		GPIO_ToggleOutputPin(GPIOD,GPIO_PIN_NO_12);
+		delay();
+	}
+	return 0;
+}
+

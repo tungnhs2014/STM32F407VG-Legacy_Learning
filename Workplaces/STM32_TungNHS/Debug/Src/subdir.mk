0f62233b5e0e2785bc54e768ee3f202dec2e05cc
tungnhs <tungnhs2014@gmail.com>
2024-10-11 22:47:30 +0700

    Add i2c src code

--- Workplaces/STM32_TungNHS/Debug/Src/subdir.mk
+++ Workplaces/STM32_TungNHS/Debug/Src/subdir.mk
@@ -5,17 +5,17 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../Src/06_spi_tx_testing.c \
+../Src/013_i2c_master_rx_testing.c \
 ../Src/syscalls.c \
 ../Src/sysmem.c 
 
 OBJS += \
-./Src/06_spi_tx_testing.o \
+./Src/013_i2c_master_rx_testing.o \
 ./Src/syscalls.o \
 ./Src/sysmem.o 
 
 C_DEPS += \
-./Src/06_spi_tx_testing.d \
+./Src/013_i2c_master_rx_testing.d \
 ./Src/syscalls.d \
 ./Src/sysmem.d 
 
@@ -27,7 +27,7 @@ Src/%.o Src/%.su Src/%.cyclo: ../Src/%.c Src/subdir.mk
 clean: clean-Src
 
 clean-Src:
-	-$(RM) ./Src/06_spi_tx_testing.cyclo ./Src/06_spi_tx_testing.d ./Src/06_spi_tx_testing.o ./Src/06_spi_tx_testing.su ./Src/syscalls.cyclo ./Src/syscalls.d ./Src/syscalls.o ./Src/syscalls.su ./Src/sysmem.cyclo ./Src/sysmem.d ./Src/sysmem.o ./Src/sysmem.su
+	-$(RM) ./Src/013_i2c_master_rx_testing.cyclo ./Src/013_i2c_master_rx_testing.d ./Src/013_i2c_master_rx_testing.o ./Src/013_i2c_master_rx_testing.su ./Src/syscalls.cyclo ./Src/syscalls.d ./Src/syscalls.o ./Src/syscalls.su ./Src/sysmem.cyclo ./Src/sysmem.d ./Src/sysmem.o ./Src/sysmem.su
 
 .PHONY: clean-Src
 
--- Workplaces/STM32_TungNHS/Drivers/Inc/stm32f407xx.h
+++ Workplaces/STM32_TungNHS/Drivers/Inc/stm32f407xx.h
@@ -615,8 +615,8 @@ typedef struct
 
 #include "stm32f407xx_gpio_driver.h"
 #include "stm32f407xx_spi_driver.h"
-//#include "stm32f407xx_i2c_driver.h"
-//#include "stm32f407xx_usart_driver.h"
-//#include "stm32f407xx_rcc_driver.h"
+#include "stm32f407xx_i2c_driver.h"
+#include "stm32f407xx_usart_driver.h"
+#include "stm32f407xx_rcc_driver.h"
 
 #endif /* INC_STM3F407XX_H_ */
